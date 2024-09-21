7c45a3f75193fc2fdb7fad8c7d5c43089d7c0189
Ryan Harris <ryan@synthesis.co.za>
2024-09-21 16:37:21 +0200

    Factorial sum func

--- main.hs
+++ main.hs
@@ -1,3 +1,8 @@
+sumtorial :: Integer -> Integer
+sumtorial 0 = 0
+sumtorial n = n + sumtorial (n-1)
+
 main = do
   putStrLn "Hello, everybody!"
   putStrLn ("Please look at my favourite odd numbers: " ++ show (filter odd [10..20]))
+  putStrLn ("Sumtorial: " ++ show (sumtorial 3))
